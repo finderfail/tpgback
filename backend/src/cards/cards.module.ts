f35f69cf7e572f896652f164b85afb0af16c1dd8
Ilya Lvov <ilyalvov2007@gmail.com>
2026-05-21 11:25:10 +0300

    орп

--- backend/src/cards/cards.module.ts
+++ backend/src/cards/cards.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CardsController } from '../app.controller.js';
+import { CardsService } from './cards.service.js';
 
-@Module({})
+
+@Module({  
+    imports: [],
+    controllers: [CardsController],
+    providers: [CardsService]
+})
 export class CardsModule {}
